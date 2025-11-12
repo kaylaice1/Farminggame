591320bb15115cb2fa82be01baefde497c65894a
KayLynn Johnson <156222957+kaylaice1@users.noreply.github.com>
2025-11-12 17:48:20 -0600

    dialogue

--- scenes/characters/guide/guide.gd
+++ scenes/characters/guide/guide.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var balloon_scene = preload("res://dialogue/game_dialogue_balloon.tscn")
 
 @onready var interactable_component: InteractableComponent = $InteractableComponent
 @onready var interactable_label_component: Control = $InteractableLabelComponent
@@ -11,6 +12,8 @@ func _ready() -> void:
 	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
 	interactable_label_component.hide()
 	
+	GameDialogueManager.give_crop_seeds.connect(on_give_crop_seeds)
+	
 func on_interactable_activated() -> void:
 	interactable_label_component.show()
 	in_range = true
@@ -18,3 +21,16 @@ func on_interactable_activated() -> void:
 func on_interactable_deactivated() -> void:
 	interactable_label_component.hide()
 	in_range = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if in_range:
+		if event.is_action_pressed("show_dialogue"):
+			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+			get_tree().current_scene.add_child(balloon)
+			balloon.start(load("res://dialogue/conversations/guide.dialogue"), "start")
+
+func on_give_crop_seeds() -> void:
+	ToolManager.enable_tool_button(DataTypes.Tools.TillGround)
+	ToolManager.enable_tool_button(DataTypes.Tools.WaterCrops)
+	ToolManager.enable_tool_button(DataTypes.Tools.PlantCorn)
+	ToolManager.enable_tool_button(DataTypes.Tools.PlantTomato)
